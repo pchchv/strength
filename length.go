22dbeeb785ae31826f5adc4d79a3a26438d45645
pchchv <ipchchv@gmail.com>
2023-05-30 12:29:59 +0300

    Implement a removeMoreThanTwoRepeatingChars func

--- length.go
+++ length.go
@@ -11,3 +11,20 @@ func deleteRuneAt(runes []rune, i int) []rune {
 
 	return runes[:len(runes)-1]
 }
+
+func removeMoreThanTwoRepeatingChars(s string) string {
+	var prevPrev rune
+	var prev rune
+	runes := []rune(s)
+	for i := 0; i < len(runes); i++ {
+		r := runes[i]
+		if r == prev && r == prevPrev {
+			runes = deleteRuneAt(runes, i)
+			i--
+		}
+		prevPrev = prev
+		prev = r
+	}
+
+	return string(runes)
+}
