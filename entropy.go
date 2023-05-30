a59838f29a48fc06d84549e3328ff13bd9bcd68c
pchchv <ipchchv@gmail.com>
2023-05-30 12:25:38 +0300

    Implement a logPow func

--- entropy.go
+++ entropy.go
@@ -9,3 +9,15 @@ func logX(base, n float64) float64 {
 	// change of base formulae
 	return math.Log2(n) / math.Log2(base)
 }
+
+// logPow calculates log_base(x^y) without
+// leaving log-space for each multiplication step,
+// allowing it to take up less memory space.
+func logPow(expBase float64, pow int, logBase float64) (total float64) {
+	// logb (MN) = logb M + logb N
+	for i := 0; i < pow; i++ {
+		total += logX(logBase, expBase)
+	}
+
+	return total
+}
