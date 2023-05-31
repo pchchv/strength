3402b7c19dcd91893f881ca37dbbfd134224fc48
pchchv <ipchchv@gmail.com>
2023-05-31 09:43:45 +0300

    Add tests of a logX func

--- entropy_test.go
+++ entropy_test.go
@@ -5,6 +5,26 @@ import (
 	"testing"
 )
 
+func TestLogX(t *testing.T) {
+	expected := math.Round(math.Log2(7) / math.Log2(2))
+	actual := math.Round(logX(2, 7))
+	if actual != expected {
+		t.Errorf("Expected %v, got %v", expected, actual)
+	}
+
+	expected = math.Round(math.Log2(10) / math.Log2(2))
+	actual = math.Round(logX(2, 10))
+	if actual != expected {
+		t.Errorf("Expected %v, got %v", expected, actual)
+	}
+
+	expected = math.Round(math.Log2(11) / math.Log2(2))
+	actual = math.Round(logX(2, 11))
+	if actual != expected {
+		t.Errorf("Expected %v, got %v", expected, actual)
+	}
+}
+
 func TestLogPow(t *testing.T) {
 	expected := math.Round(math.Log2(math.Pow(7, 8)))
 	actual := math.Round(logPow(7, 8, 2))
