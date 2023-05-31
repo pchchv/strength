372fc5f9b9e89ade38d3e71df7e4c30138706b93
pchchv <ipchchv@gmail.com>
2023-05-31 09:36:05 +0300

    Add test of a getBase func

---
+++ base_test.go
@@ -0,0 +1,53 @@
+package strength
+
+import "testing"
+
+func TestGetBase(t *testing.T) {
+	actual := getBase("abcd")
+	expected := len(lowerChars)
+	if actual != expected {
+		t.Errorf("Wanted %v, got %v", expected, actual)
+	}
+
+	actual = getBase("abcdA")
+	expected = len(lowerChars) + len(upperChars)
+	if actual != expected {
+		t.Errorf("Wanted %v, got %v", expected, actual)
+	}
+
+	actual = getBase("A")
+	expected = len(upperChars)
+	if actual != expected {
+		t.Errorf("Wanted %v, got %v", expected, actual)
+	}
+
+	actual = getBase("^_")
+	expected = len(otherSpecialChars) + len(sepChars)
+	if actual != expected {
+		t.Errorf("Wanted %v, got %v", expected, actual)
+	}
+
+	actual = getBase("^")
+	expected = len(otherSpecialChars)
+	if actual != expected {
+		t.Errorf("Wanted %v, got %v", expected, actual)
+	}
+
+	actual = getBase("!")
+	expected = len(replaceChars)
+	if actual != expected {
+		t.Errorf("Wanted %v, got %v", expected, actual)
+	}
+
+	actual = getBase("123")
+	expected = len(digitsChars)
+	if actual != expected {
+		t.Errorf("Wanted %v, got %v", expected, actual)
+	}
+
+	actual = getBase("123ü")
+	expected = len(digitsChars) + 1
+	if actual != expected {
+		t.Errorf("Wanted %v, got %v", expected, actual)
+	}
+}
